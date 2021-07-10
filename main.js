07dbc51f369cf95bea243040d0a553ff3cb21f01
Sara Fishburn <fishburn.sara@gmail.com>
2021-07-10 11:33:17 +1000

    installed nodemon

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
+//import express from 'express'
+
 const express = require("express")
 const app = express()
+
+app.get("/", () => {
+  console.log("Hit the root route")
+})
+// app.post()
+// app.put/patch
+// app.delete
+
+app.listen(5000, () => console.log("Running on port 5000"))
